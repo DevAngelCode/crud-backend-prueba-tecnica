83db1f4e66b532dbc14cc0f2d6afbb608582679c
dev <240357148+DevAngelCode@users.noreply.github.com>
2026-09-07 17:31:12 -0500

    inciando el crud para customers parte backend

---
+++ src/main/java/com/crud_backend_prueba_tecnica/app/CrudBackendPruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.crud_backend_prueba_tecnica.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBackendPruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBackendPruebaTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud_backend_prueba_tecnica/app/CrudBackendPruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_backend_prueba_tecnica.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBackendPruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
